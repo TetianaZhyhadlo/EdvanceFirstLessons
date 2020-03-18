e75e8dcea26c0631cddc852f12c8df72dd968756
TetianaZhyhadlo <61022475+TetianaZhyhadlo@users.noreply.github.com>
2020-03-18 23:08:57 +0200

    Add files via upload
    old

---
+++ Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EdvanceC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Book a = new Book();
+                a.Name = "Как построить идеальный мир без зависти, лжи, злости и ненависти.";
+                
+            }
+            catch (BookException except)
+            {
+                Console.WriteLine("Ошибка: " + except.Message);
+            }
+            Console.Read();
+        }
+    }
+}
